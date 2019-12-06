31b3c6514daa862c0853568059eee7abbf5fd78b
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2019-12-06 10:06:28 -0700

    app front end setup complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Form from './Components/Form/Form';
+import Header from './Components/Header/Header';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <div className="App">
+          <Header />
+          <Dashboard />
+          <Form />
+      </div>
+    )
+  }
 }
 
 export default App;
